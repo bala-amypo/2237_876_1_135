993f922f82fcbed90286ee9b0736cb5cf933bb40
bala-amypo <bala@amypo.in>
2025-12-20 16:59:21 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -7,16 +7,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+
 
 
 @RestControllerAdvice
 public class GlobalExceptionHandler 
 {
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(),
-        HttpStatus.NOT_FOUND);
-    }
+    // @ExceptionHandler(ResourceNotFoundException.class)
+    // public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex) {
+    //     return new ResponseEntity<>(ex.getMessage(),
+    //     HttpStatus.NOT_FOUND);
+    // }
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
