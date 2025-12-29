97e5125c85028b15f8cd087f339fdcf7d692ab04
bala-amypo <bala@amypo.in>
2025-12-29 04:09:51 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/SubscriptionServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/SubscriptionServiceImpl.java
@@ -57,16 +57,6 @@ public class SubscriptionServiceImpl implements SubscriptionService {
     @Override
     public void unsubscribe(Long userId, Long eventId) {
 
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
-        
-        if (user.getRole() == Role.PUBLISHER) {
-            throw new ResponseStatusException(
-                    HttpStatus.FORBIDDEN,
-                    "Publisher cannot subscribe the event"
-            );
-        }
-
         Subscription subscription = subscriptionRepository
                 .findByUserIdAndEventId(userId, eventId)
                 .orElseThrow(() -> new ResourceNotFoundException("Subscription not found"));
